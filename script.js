c94f0afbfe0a57a5f1715e08c56b5845f1957e8a
jasvinderkhera <ankitkhera15@gmail.com>
2024-07-27 22:48:15 +0530

    swiperjs Carousel implemented instead of slick slider

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".carousel").slick({
-        slidesToShow: 1,
-        infinity: true,
-        autoplay: true,
-        arrow:true,
-        autoplaySpeed: 5000,
-    })
-})
