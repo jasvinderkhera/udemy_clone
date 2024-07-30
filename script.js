d7dc244829a00973412bb40029901c9c60cdccb9
jasvinderkhera <ankitkhera15@gmail.com>
2024-07-30 21:16:16 +0530

    pills and tabs corrected

---
+++ script.js
@@ -0,0 +1,49 @@
+let parent_width = $(".cat_box").width()
+let child_width = $(".sub_categories").width()
+let translate = 0
+let max = child_width - parent_width
+
+function setWidth(){
+
+}
+//max translate = child_width - parent_width
+function left(button){
+    if(translate<=0){
+        $(button).hide()
+        $(button).parent().parent().find(".right").show()
+    }else{
+        translate -=50
+    }
+    
+    console.log(parent_width,child_width)
+  // $(".child").css("transform","translateX("+translate+"px)")
+  $(button).parent().parent().find(".cat_box").scrollLeft(translate)
+}
+function right(button){
+    if(translate>=max){
+        $(button).hide()
+        
+        $(button).parent().parent().find(".left").show()
+        
+    }else{
+        translate +=50
+    }
+    
+    console.log(parent_width,child_width)
+  // $(".child").css("transform","translateX("+translate+"px)")
+  $(button).parent().parent().find(".cat_box").scrollLeft(translate)
+    
+}
+
+
+
+
+// Nav Tab js
+
+
+var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
