3c7963c52477c9e3210734d280d80e164c4e631e
jasvinderkhera <ankitkhera15@gmail.com>
2024-08-06 18:30:19 +0530

    main page tooltip remaining

--- categories2.js
+++ categories2.js
@@ -136,3 +136,163 @@ wrapper_web.innerHTML+=`
 
 `
 })
+
+
+
+let js_data = [
+  {
+    thumbnail:"https://img-c.udemycdn.com/course/240x135/851712_fc61_6.jpg",
+        title:"The Complete Javascript Course 2024: From Zero to Expert!",
+        tutor:"Jonas Schmedtmann",
+        rating:'<span class="rating_size">4.7</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+        rating_count:"(210,504)",
+        price:"₹4,099",
+         category:"javascript"
+  },
+  {
+    thumbnail:"https://img-c.udemycdn.com/course/240x135/2272508_45da_3.jpg",
+        title:"The Complete Full-Stack JavaScript Course",
+        tutor:"Joseph Delgadillo, Nick Germaine",
+        rating:'<span class="rating_size">4.3</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+        rating_count:"(4,168)",
+        price:"₹2,699",
+         category:"javascript"
+  },
+
+  {thumbnail:"https://img-c.udemycdn.com/course/240x135/1565838_e54e_18.jpg",
+    title:"The Complete 2024 Web Development Bootcamp",
+ tutor:"Dr.Angela Yu,Developer and Lead...",
+ rating:'4.7  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+ rating_count:"(392,341)",
+ price:"₹3,099",
+ category:"web development"
+},
+
+{thumbnail:"https://img-c.udemycdn.com/course/240x135/625204_436a_3.jpg",
+    title:"The Web Developer Bootcamp 2024",
+    tutor:"Colt Steele",
+    rating:'4.7  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(274,277)",
+    price:"₹3,499",
+     category:"web development"
+   },
+
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/965528_737d_7.jpg",
+    title:"Web Development Masterclass - Online Certification Course",
+    tutor:"YouAccel Training",
+    rating:'4.7  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(9,875)",
+    price:"₹3,099",
+     category:"web development"
+   },
+
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/1415652_ad99_4.jpg",
+    title:"Practical Web Development: 22 Courses in 1",
+    tutor:"Creative Online School ",
+    rating:'4.2  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>',
+    rating_count:"(4,329)",
+    price:"₹3,699",
+     category:"web development"
+   },
+
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/764164_de03_5.jpg",
+    title:"The Complete Web Developer Course 3.0",
+    tutor:"Rob Percival, Codestars • over 2 millio...",
+    rating:'4.5  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(72,180)",
+    price:"₹4,999",
+     category:"web development"
+   },
+
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/2334226_3436_3.jpg",
+    title:"Internet and Web Development Fundamentals",
+    tutor:"YouAccel Training",
+    rating:'4.5  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(1,708)",
+    price:"₹1,999",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/2488654_e156_3.jpg",
+    title:"The Complete 2023 PHP Full Stack Web Developer Bootcamp",
+    tutor:"Srini Vanmala",
+    rating:'<span class="rating_size">4.7</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(5,350)",
+    price:"₹6,500",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/881980_fc8f.jpg",
+    title:"Pre-Programming: Everything you need to know before you code",
+    tutor:"Evan Kimbrell",
+    rating:'<span class="rating_size">4.6</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(12,235)",
+    price:"₹4,099",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/19332_6b49_15.jpg",
+    title:"The Complete 2020 Fullstack Web Developer Course",
+    tutor:"Kalob Taulien",
+    rating:'<span class="rating_size">4.5</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(7,431)",
+    price:"₹4,099",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/4548492_0942.jpg",
+    title:"CSS, Bootstrap ,JavaScript, Web Development Course",
+    tutor:"PROPER DOT INSTITUTE",
+    rating:'<span class="rating_size">4.3</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(1,113)",
+    price:"₹2,499",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/1218586_9f86.jpg",
+    title:"The Advanced Web Developer Bootcamp",
+    tutor:"Colt Steele, Elie Schoppik, Tim Garcia,...",
+    rating:'<span class="rating_size">4.6</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(9,332)",
+    price:"₹3,499",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/446134_383c_3.jpg",
+    title:"Ultimate Web Designer & Web Developer Course",
+    tutor:"Brad Hussey",
+    rating:'<span class="rating_size">4.7</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>',
+    rating_count:"(14,488)",
+    price:"₹4,099",
+     category:"web development"
+   },
+   {thumbnail:"https://img-c.udemycdn.com/course/240x135/4743940_53b1_6.jpg",
+    title:"The Full Stack Web Development Bootcamp (Frontend & Backend)",
+    tutor:"Faizal Zamir, Jafri Code",
+    rating:'<span class="rating_size">4.7</span>  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>',
+    rating_count:"(447)",
+    price:"₹2,699",
+     category:"web development"
+   },
+]
+
+
+
+let wrapper_js = document.querySelector(".wrapper_js")
+js_data.forEach(item => {
+wrapper_js.innerHTML+=`
+<div class="con swiper-slide">
+                  <div class="swiper_overlay_1 border border-2">
+                    <div class="img_box">
+                      <img
+                        src="${item.thumbnail}"
+                        class="slide-image"
+                      />
+                    </div>
+                    <div class="detail_box px-4 py-2">
+                      <h6 class="item_title">${item.title}</h6>
+                      <span class="text-secondary">${item.tutor}</span>
+                      <p class="m-0 mb-2 fw-bold">${item.rating}
+                        <span class="text-secondary rating_count fw-light"> ${item.rating_count}</span>
+                      </p>
+                      <span class="course_price">${item.price}</span>
+                    </div>
+                  </div>
+                </div>
+
+`
+})
