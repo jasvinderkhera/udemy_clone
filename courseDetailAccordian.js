2893ddbf51bba46d592463c818b42e01be1420bd
jasvinderkhera <ankitkhera15@gmail.com>
2024-08-11 14:17:27 +0530

    some accordian data created

---
+++ courseDetailAccordian.js
@@ -0,0 +1,52 @@
+let accordionData = [
+    {
+        td_1:"fa-regular fa-file",
+        td_2:`<a href="">A Note About 2023 Course Updates</a>`,
+        td_3:`<a href="">Preview</a>`,
+        td_4:"00:36"
+    },
+    
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`<a href="">What is HTML?</a>`,
+        td_3:`<a href="">Preview</a>`,
+        td_4:"04:18"
+    },
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`How to Download the Course Resources`,
+        td_3:``,
+        td_4:"02:43"
+    },
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`HTML Heading Elements`,
+        td_3:``,
+        td_4:"14:24"
+    },
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`HTML Paragraph Elements`,
+        td_3:``,
+        td_4:"08:40"
+    },
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`Self Closing Tags`,
+        td_3:``,
+        td_4:"11:40"
+    },
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`[Project] Movie Ranking`,
+        td_3:``,
+        td_4:"05:43"
+    },
+    {
+        td_1:"fa-solid fa-tv",
+        td_2:`How to Ace this Course`,
+        td_3:``,
+        td_4:"01:24"
+    },
+]
+
