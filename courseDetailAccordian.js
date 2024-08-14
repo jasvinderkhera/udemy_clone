f7322b45b415ff0f0e0712e90d45b669ef511c30
jasvinderkhera <ankitkhera15@gmail.com>
2024-08-14 18:38:07 +0530

    accordian problem solved

--- courseDetailAccordian.js
+++ courseDetailAccordian.js
@@ -1,364 +1,406 @@
 let accordionData = [
-    {
-        td_1: "fa-regular fa-file",
-        td_2: `<a href="">A Note About 2023 Course Updates</a>`,
-        td_3: `<a href="">Preview</a>`,
-        td_4: "00:36"
+    {"title":"Introduction to HTML",
+        "data":[{
+        "td_1": "fa-regular fa-file",
+        "td_2": `<a href="">A Note About 2023 Course Updates</a>`,
+        "td_3": `<a href="">Preview</a>`,
+        "td_4": "00:36"
     },
 
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="">What is HTML?</a>`,
-        td_3: `<a href="">Preview</a>`,
-        td_4: "04:18"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `<a href="">What is HTML?</a>`,
+        "td_3": `<a href="">Preview</a>`,
+        "td_4": "04:18"
     },
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `How to Download the Course Resources`,
-        td_3: ``,
-        td_4: "02:43"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `How to Download the Course Resources`,
+        "td_3": ``,
+        "td_4": "02:43"
     },
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `HTML Heading Elements`,
-        td_3: ``,
-        td_4: "14:24"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `HTML Heading Elements`,
+        "td_3": ``,
+        "td_4": "14:24"
     },
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `HTML Paragraph Elements`,
-        td_3: ``,
-        td_4: "08:40"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `HTML Paragraph Elements`,
+        "td_3": ``,
+        "td_4": "08:40"
     },
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `Self Closing Tags`,
-        td_3: ``,
-        td_4: "11:40"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `Self Closing Tags`,
+        "td_3": ``,
+        "td_4": "11:40"
     },
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Movie Ranking`,
-        td_3: ``,
-        td_4: "05:43"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `[Project] Movie Ranking`,
+        "td_3": ``,
+        "td_4": "05:43"
     },
     {
-        td_1: "fa-solid fa-tv",
-        td_2: `How to Ace this Course`,
-        td_3: ``,
-        td_4: "01:24"
+        "td_1": "fa-solid fa-tv",
+        "td_2": `How to Ace this Course`,
+        "td_3": ``,
+        "td_4": "01:24"
+    },],
+    "lectures":"8 lectures",
+    "duration":"49min"},
+
+    {
+        "title":"Intermediate HTML",
+        "data":[
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `The List Element`,
+                "td_3": ``,
+                "td_4": "10:32"
+            },
+        
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Nesting and Indentation`,
+                "td_3": ``,
+                "td_4": "14:09"
+            },
+        
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Anchor Elements`,
+                "td_3": ``,
+                "td_4": "10:45"
+            },
+        
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Image Elements`,
+                "td_3": ``,
+                "td_4": "08:17"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `[Project] Birthday Invite`,
+                "td_3": ``,
+                "td_4": "04:01"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Tip from Angela - Habit Building with the Calendar Trick`,
+                "td_3": ``,
+                "td_4": "02:52"
+            },
+            {
+                "td_1": "fa-regular fa-file",
+                "td_2": `Get Access to the Monthly App Brewery Newsletter`,
+                "td_3": ``,
+                "td_4": "00:56"
+            },
+        ],
+        "lectures":"7 lectures",
+    "duration":"52min"
     },
+
+    {
+        "title":"Multi-Page Websites",
+        "data":[
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Computer File Paths`,
+                "td_3": ``,
+                "td_4": "19:20"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `What are Webpages?`,
+                "td_3": ``,
+                "td_4": "12:55"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `The HTML Boilerplate`,
+                "td_3": ``,
+                "td_4": "12:53"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `[Project] Portfolio Website`,
+                "td_3": ``,
+                "td_4": "08:34"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `How to Host Your Website for Free with GitHub`,
+                "td_3": ``,
+                "td_4": "08:33"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `<a href="">Introduction to Capstone Projects</a>`,
+                "td_3": `<a href="">Preview</a>`,
+                "td_4": "05:05"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Instructions to Capstone Project 1 - Online Resume`,
+                "td_3": ``,
+                "td_4": "02:22"
+            },
+        ],
+        "lectures":"7 lectures",
+    "duration":"1hr 10min"
+    },
+    {"title":"Introduction to CSS",
+    "data":[
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `Why do we need CSS?`,
+            "td_3": ``,
+            "td_4": "08:44"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `How to add CSS`,
+            "td_3": ``,
+            "td_4": "15:19"
+        },
+        {
+            "td_1": "fa-regular fa-lightbulb",
+            "td_2": `CSS Quiz`,
+            "td_3": ``,
+            "td_4": "3 questions"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `<a href="" class="blue_shade_nav">CSS Selectors</a>`,
+            "td_3": `<a href="" class="blue_shade_nav">Preview</a>`,
+            "td_4": "22:56"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `[Project] Color Vocab Website`,
+            "td_3": ``,
+            "td_4": "09:08"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `Tip from Angela - Dealing with Distractions`,
+            "td_3": ``,
+            "td_4": "02:28"
+        },
+        {
+            "td_1": "fa-regular fa-file",
+            "td_2": `Join the Student Community`,
+            "td_3": ``,
+            "td_4": "00:39"
+        },
+    ],
+     "lectures":"6 lectures",
+    "duration":"59min"
+    },
+    {
+        "title":"CSS Properties",
+        "data":[
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `CSS Colours`,
+                "td_3": ``,
+                "td_4": "07:05"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Font Properties`,
+                "td_3": ``,
+                "td_4": "21:26"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Inspecting CSS`,
+                "td_3": ``,
+                "td_4": "11:52"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `The CSS Box Model - Margin, Padding and Border`,
+                "td_3": ``,
+                "td_4": "20:45"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `[Project] Motivational Poster Website`,
+                "td_3": ``,
+                "td_4": "07:37"
+            },
+        ],
+              "lectures":"5 lectures",
+    "duration":"1hr 9min"
+    },
+    {
+        "title":"Intermediate CSS",
+        "data":[
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `<a href="" class="blue_shade_nav">The Cascade - Specificity and Inheritance</a>`,
+                "td_3": `<a href="" class="blue_shade_nav">Preview</a>`,
+                "td_4": "25:28"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Combining CSS Selectors`,
+                "td_3": ``,
+                "td_4": "23:12"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `CSS Positioning`,
+                "td_3": ``,
+                "td_4": "22:50"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `[Project] CSS Flag`,
+                "td_3": ``,
+                "td_4": "17:54"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Tip from Angela - Nothing Easy is Worth Doing!`,
+                "td_3": ``,
+                "td_4": "03:35"
+            },
+        ],
+        "lectures":"5 lectures",
+    "duration":"1hr 33min"
+    },
+    {
+    "title":"Advanced CSS",
+    "data": [
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `CSS Display`,
+            "td_3": ``,
+            "td_4": "11:30"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `CSS Float`,
+            "td_3": ``,
+            "td_4": "11:39"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `<a href="" class="blue_shade_nav">How to Create Responsive Websites</a>`,
+            "td_3": `<a href="" class="blue_shade_nav">Preview</a>`,
+            "td_4": "18:44"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `Media Queries`,
+            "td_3": ``,
+            "td_4": "10:32"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `[Project] Web Design Agency Website`,
+            "td_3": ``,
+            "td_4": "07:06"
+        },
+        {
+            "td_1": "fa-solid fa-tv",
+            "td_2": `Tip from Angela - How to Deal with Procrastination`,
+            "td_3": ``,
+            "td_4": "04:08"
+        },
+    ],
+     "lectures":"6 lectures",
+    "duration":"1hr 4min"
+    },
+    {
+        "title":"Flexbox",
+        "data":[
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `<a href="" class="blue_shade_nav">Display:Flex</a>`,
+                "td_3": `<a href="" class="blue_shade_nav">Preview</a>`,
+                "td_4": "14:21"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `<a href="" class="blue_shade_nav">Flex Direction</a>`,
+                "td_3": `<a href="" class="blue_shade_nav">Preview</a>`,
+                "td_4": "13:43"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Flex Layout`,
+                "td_3": ``,
+                "td_4": "19:22"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Flex Sizing`,
+                "td_3": ``,
+                "td_4": "24:27"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `[Project] Pricing Table`,
+                "td_3": ``,
+                "td_4": "10:08"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Tip from Angela - Building a Programming Habit`,
+                "td_3": ``,
+                "td_4": "02:46"
+            },
+        ],
+         "lectures":"6 lectures",
+    "duration":"1hr 25min"
+    },
+    {
+        "title":"Grid",
+        "data":[
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Display: Grid`,
+                "td_3": ``,
+                "td_4": "14:55"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Grid Sizing`,
+                "td_3": ``,
+                "td_4": "23:32"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `Grid Placement`,
+                "td_3": ``,
+                "td_4": "26:17"
+            },
+            {
+                "td_1": "fa-solid fa-tv",
+                "td_2": `[Project] Mondrian Painting`,
+                "td_3": ``,
+                "td_4": "06:45"
+            },
+        ],
+        "lectures":"4 lectures",
+    "duration":"1hr 11min"
+    }
 ]
 
-let accordionData_2 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `The List Element`,
-        td_3: ``,
-        td_4: "10:32"
-    },
 
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Nesting and Indentation`,
-        td_3: ``,
-        td_4: "14:09"
-    },
 
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Anchor Elements`,
-        td_3: ``,
-        td_4: "10:45"
-    },
 
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Image Elements`,
-        td_3: ``,
-        td_4: "08:17"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Birthday Invite`,
-        td_3: ``,
-        td_4: "04:01"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Tip from Angela - Habit Building with the Calendar Trick`,
-        td_3: ``,
-        td_4: "02:52"
-    },
-    {
-        td_1: "fa-regular fa-file",
-        td_2: `Get Access to the Monthly App Brewery Newsletter`,
-        td_3: ``,
-        td_4: "00:56"
-    },
 
-]
 
-let accordionData_3 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Computer File Paths`,
-        td_3: ``,
-        td_4: "19:20"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `What are Webpages?`,
-        td_3: ``,
-        td_4: "12:55"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `The HTML Boilerplate`,
-        td_3: ``,
-        td_4: "12:53"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Portfolio Website`,
-        td_3: ``,
-        td_4: "08:34"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `How to Host Your Website for Free with GitHub`,
-        td_3: ``,
-        td_4: "08:33"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="">Introduction to Capstone Projects</a>`,
-        td_3: `<a href="">Preview</a>`,
-        td_4: "05:05"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Instructions to Capstone Project 1 - Online Resume`,
-        td_3: ``,
-        td_4: "02:22"
-    },
-]
 
-let accordionData_4 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Why do we need CSS?`,
-        td_3: ``,
-        td_4: "08:44"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `How to add CSS`,
-        td_3: ``,
-        td_4: "15:19"
-    },
-    {
-        td_1: "fa-regular fa-lightbulb",
-        td_2: `CSS Quiz`,
-        td_3: ``,
-        td_4: "3 questions"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="" class="blue_shade_nav">CSS Selectors</a>`,
-        td_3: `<a href="" class="blue_shade_nav">Preview</a>`,
-        td_4: "22:56"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Color Vocab Website`,
-        td_3: ``,
-        td_4: "09:08"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Tip from Angela - Dealing with Distractions`,
-        td_3: ``,
-        td_4: "02:28"
-    },
-    {
-        td_1: "fa-regular fa-file",
-        td_2: `Join the Student Community`,
-        td_3: ``,
-        td_4: "00:39"
-    },
-]
-
-let accordionData_5 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `CSS Colours`,
-        td_3: ``,
-        td_4: "07:05"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Font Properties`,
-        td_3: ``,
-        td_4: "21:26"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Inspecting CSS`,
-        td_3: ``,
-        td_4: "11:52"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `The CSS Box Model - Margin, Padding and Border`,
-        td_3: ``,
-        td_4: "20:45"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Motivational Poster Website`,
-        td_3: ``,
-        td_4: "07:37"
-    },
-]
-
-let accordionData_6 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="" class="blue_shade_nav">The Cascade - Specificity and Inheritance</a>`,
-        td_3: `<a href="" class="blue_shade_nav">Preview</a>`,
-        td_4: "25:28"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Combining CSS Selectors`,
-        td_3: ``,
-        td_4: "23:12"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `CSS Positioning`,
-        td_3: ``,
-        td_4: "22:50"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] CSS Flag`,
-        td_3: ``,
-        td_4: "17:54"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Tip from Angela - Nothing Easy is Worth Doing!`,
-        td_3: ``,
-        td_4: "03:35"
-    },
-]
-
-let accordionData_7 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `CSS Display`,
-        td_3: ``,
-        td_4: "11:30"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `CSS Float`,
-        td_3: ``,
-        td_4: "11:39"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="" class="blue_shade_nav">How to Create Responsive Websites</a>`,
-        td_3: `<a href="" class="blue_shade_nav">Preview</a>`,
-        td_4: "18:44"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Media Queries`,
-        td_3: ``,
-        td_4: "10:32"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Web Design Agency Website`,
-        td_3: ``,
-        td_4: "07:06"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Tip from Angela - How to Deal with Procrastination`,
-        td_3: ``,
-        td_4: "04:08"
-    },
-]
-
-let accordionData_8 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="" class="blue_shade_nav">Display:Flex</a>`,
-        td_3: `<a href="" class="blue_shade_nav">Preview</a>`,
-        td_4: "14:21"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `<a href="" class="blue_shade_nav">Flex Direction</a>`,
-        td_3: `<a href="" class="blue_shade_nav">Preview</a>`,
-        td_4: "13:43"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Flex Layout`,
-        td_3: ``,
-        td_4: "19:22"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Flex Sizing`,
-        td_3: ``,
-        td_4: "24:27"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Pricing Table`,
-        td_3: ``,
-        td_4: "10:08"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Tip from Angela - Building a Programming Habit`,
-        td_3: ``,
-        td_4: "02:46"
-    },
-]
-
-let accordionData_9 = [
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Display: Grid`,
-        td_3: ``,
-        td_4: "14:55"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Grid Sizing`,
-        td_3: ``,
-        td_4: "23:32"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `Grid Placement`,
-        td_3: ``,
-        td_4: "26:17"
-    },
-    {
-        td_1: "fa-solid fa-tv",
-        td_2: `[Project] Mondrian Painting`,
-        td_3: ``,
-        td_4: "06:45"
-    },
-]
 
 
 
